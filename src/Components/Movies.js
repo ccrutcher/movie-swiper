b42a5f8a385498624ea523ccd7944d3c401fbc2e
Cole <colejcrutcher@gmail.com>
2021-04-23 17:58:33 -0500

    Initial commit

---
+++ src/Components/Movies.js
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react';
+
+
+const Movies = () => {
+    const [movies, setMovies] = useState([]);
+
+    require('dotenv').config();
+    
+    //Fetch api and set movies state to return data
+    useEffect(() => {
+        
+
+
+    }, [])
+
+    return(
+        <div>
+        </div>
+    );
+}
+
+export default Movies;
